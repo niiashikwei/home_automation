f6c05e630d07798ed47a9ae3e19b26c36fe4fae8
Nii <niiashikwei@hotmail.com>
2014-04-05 15:20:31 -0400

    Nii - got basic reveal js app working

---
+++ angular_leap_presentations/js/module.js
@@ -0,0 +1,7 @@
+angular.module("angularRevealSlides", [])
+    .run(function($rootScope){
+        $rootScope.left = Reveal.left;
+        $rootScope.up = Reveal.up;
+        $rootScope.down = Reveal.down;
+        $rootScope.right = Reveal.right;
+    });
