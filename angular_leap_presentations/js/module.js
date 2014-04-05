0f77685f0c18177a3cbdc6239677364a8c505ba2
Nii <niiashikwei@hotmail.com>
2014-04-05 17:25:31 -0400

    Nii - following angular-leap tutorial, almost there but something missing

--- angular_leap_presentations/js/module.js
+++ angular_leap_presentations/js/module.js
@@ -1,7 +1,1 @@
-angular.module("angularRevealSlides", [])
-    .run(function($rootScope){
-        $rootScope.left = Reveal.left;
-        $rootScope.up = Reveal.up;
-        $rootScope.down = Reveal.down;
-        $rootScope.right = Reveal.right;
-    });
+angular.module("angularRevealSlides", ['angularLeap']);
